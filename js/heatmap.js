60be07a685a9c26d652d74a630c400841779ba3d
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-21 13:28:38 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -7,7 +7,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map)
 
 
-// default map settings
-function defaultMapSettings() {
- map.setView([58.373523, 26.716045], 12)
+addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+// add geoJSON layer
+async function addGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const heatData = data.features.map(heatDataConvert)
+ const heatMap = L.heatLayer(heatData, { radius: 10 })
+ heatMap.addTo(map)
 }
+// prepare spatial data for Leaflet heat
+function heatDataConvert(feature) {
+ return [
+ feature.geometry.coordinates[1],
+ feature.geometry.coordinates[0],
+ feature.properties.area,
+ ]
+}
+
