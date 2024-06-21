fd4f533d1be3f668b077fe8ee1070fa5d6927b69
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-21 11:59:00 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,1 +1,8 @@
+addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+// add geoJSON layer
+async function addGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ console.log(data.features[0]) 
+}
 
